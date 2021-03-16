41aef1a6cb0acad27cf68390ce4f7344da366a9c
Abhi@@@2607 <jainabhishekgwl904@gmail.com>
2021-03-16 12:51:13 +0530

    #1

--- upload/extension/opencart/admin/controller/report/product_purchased.php
+++ upload/extension/opencart/admin/controller/report/product_purchased.php
@@ -204,22 +204,33 @@ class ProductPurchased extends \Opencart\System\Engine\Controller {
 			'start'                  => ($page - 1) * $this->config->get('config_pagination'),
 			'limit'                  => $this->config->get('config_pagination')
 		];
-                
-                if(isset($this->request->get['download']) && !empty($this->request->get['download'])){
-                    $this->load->model('extension/opencart/report/sale');
-                    $this->model_extension_opencart_report_sale->download($filter_data,'purchased_report','Purchased','purchased_report');
-                }
 
 		$product_total = $this->model_extension_opencart_report_product->getTotalPurchased($filter_data);
 
-		$results = $this->model_extension_opencart_report_product->getPurchased($filter_data);
+		$results = $this->model_extension_opencart_report_product->getInvoicePurchased($filter_data);
 
 		foreach ($results as $result) {
+                    $order_date = date_parse($result['date_added']);
+                    
+                    $monthNum = sprintf("%02s", $order_date["month"]);
+                    $monthName = date("M", strtotime($monthNum));
+                    $invoice_date = date("d-M-y", strtotime($result['date_added']));
+                    $year = $order_date["year"];
+                    $customer_name = $result['firstname'].' '.$result['lastname'];
+                    $company = $result['company'];
+                    $gst = $result['gst'];
+                    
 			$data['products'][] = [
-				'name'     => $result['name'],
-				'model'    => $result['model'],
-				'quantity' => $result['quantity'],
-				'total'    => $this->currency->format($result['total'], $this->config->get('config_currency'))
+				'name'            => $result['name'],
+                                'month'           => $monthName,
+                                'year'            => $year,
+                                'invoice_date'    => $invoice_date,
+                                'customer_name'   => $customer_name,
+                                'company'         => $company,
+                                'gst'             => $gst,
+				'model'           => $result['model'],
+				'quantity'        => $result['quantity'],
+				'total'           => $this->currency->format($result['total'], $this->config->get('config_currency'))
 			];
 		}
 
--- upload/extension/opencart/admin/model/report/product.php
+++ upload/extension/opencart/admin/model/report/product.php
@@ -72,6 +72,43 @@ class Product extends \Opencart\System\Engine\Model {
 
 		return $query->rows;
 	}
+        
+        public function getInvoicePurchased(array $data = []): array {
+            
+		$sql = "SELECT c.firstname, c.lastname, c.company, c.gst, o.date_added, op.`name`, op.`model`, SUM(op.`quantity`) AS quantity, SUM((op.`price` + op.`tax`) * op.`quantity`) AS `total` FROM `" . DB_PREFIX . "order_product` op LEFT JOIN `" . DB_PREFIX . "order` o ON (op.`order_id` = o.`order_id`) LEFT JOIN `" . DB_PREFIX . "customer` c ON (o.`customer_id` = c.`customer_id`)";
+               
+		if (!empty($data['filter_order_status_id'])) {
+			$sql .= " WHERE o.`order_status_id` = '" . (int)$data['filter_order_status_id'] . "'";
+		} else {
+			$sql .= " WHERE o.`order_status_id` > '0'";
+		}
+
+		if (!empty($data['filter_date_start'])) {
+			$sql .= " AND DATE(o.`date_added`) >= '" . $this->db->escape((string)$data['filter_date_start']) . "'";
+		}
+
+		if (!empty($data['filter_date_end'])) {
+			$sql .= " AND DATE(o.`date_added`) <= '" . $this->db->escape((string)$data['filter_date_end']) . "'";
+		}
+
+		$sql .= " GROUP BY op.`product_id` ORDER BY total DESC";
+
+		if (isset($data['start']) || isset($data['limit'])) {
+			if ($data['start'] < 0) {
+				$data['start'] = 0;
+			}
+
+			if ($data['limit'] < 1) {
+				$data['limit'] = 20;
+			}
+
+			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+		}
+
+		$query = $this->db->query($sql);
+
+		return $query->rows;
+	}
 
 	public function getTotalPurchased(array $data = []): int {
 		$sql = "SELECT COUNT(DISTINCT op.`product_id`) AS `total` FROM `" . DB_PREFIX . "order_product` op LEFT JOIN `" . DB_PREFIX . "order` o ON (op.`order_id` = o.`order_id`)";
